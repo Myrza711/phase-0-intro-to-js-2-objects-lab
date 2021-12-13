6c0b6f5e33cd6b79a91da76a6a15c828c1379719
Askat Moldobaev <kyrgyz_man@icloud.com>
2021-12-12 20:58:44 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+    name: "Almaz",
+    streetAddress: "312 Frunze Street, Bishkek, Kyrgyzstan"
+}
+function updateEmployeeWithKeyAndValue(employee, streetAddress, newAddress) {
+    const newEmployee = {...employee}
+    newEmployee[streetAddress] = newAddress
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, newAddress) {
+    employee [streetAddress] = newAddress
+    return employee
+}
+function deleteFromEmployeeByKey(employee, name) {
+    const delEmployee = {...employee}
+    delete delEmployee.name
+    return delEmployee
+}
+console.log(employee)
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name
+    return employee
+}
+console.log(employee)
